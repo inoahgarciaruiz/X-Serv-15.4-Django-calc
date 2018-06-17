23f018baedf2e719c56230005c1fd506135924a0
inoahgarciaruiz <inoahgarciaruiz@gmail.com>
2018-06-17 07:45:21 +0200

    done

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+
+def default(request):
+    return HttpResponse('<h1>Wellcome!</h1><br><body>' +
+                        'Write your operation in the url</body>')
+
+
+def sum(request, op1, op2):
+    result = int(op1) + int(op2)
+    return HttpResponse(('<head><b>Result: </b><br></head><body>' +
+                         op1 + ' + ' + op2 + ' = ' + str(result) +
+                         '</body>'))
+
+
+def subs(request, op1, op2):
+    result = int(op1) - int(op2)
+    return HttpResponse(('<head><b>Result: </b><br></head><body>' +
+                         op1 + ' - ' + op2 + ' = ' + str(result) +
+                         '</body>'))
+
+
+def mult(request, op1, op2):
+    result = int(op1) * int(op2)
+    return HttpResponse(('<head><b>Result: </b><br></head><body>' +
+                         op1 + ' * ' + op2 + ' = ' + str(result) +
+                         '</body>'))
+
+
+def div(request, op1, op2):
+    result = int(op1) / int(op2)
+    return HttpResponse(('<head><b>Result: </b><br></head><body>' +
+                         op1 + ' / ' + op2 + ' = ' + str(result) +
+                         '</body>'))
+
+
+def notFound(request):
+    return HttpResponseNotFound('<h1>404 - Resource not Found</h1>')
--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from calc import views
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = (
+    url(r'^$', views.default),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^(\d+)\+(\d+)$', views.sum),
+    url(r'^(\d+)\-(\d+)$', views.subs),
+    url(r'^(\d+)\*(\d+)$', views.mult),
+    url(r'^(\d+)\/(\d+)$', views.div),
+    url(r'', views.notFound),
 )
